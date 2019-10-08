859fefd8198043e28abe9d1410ef2dedfc4ef358
rainy-me <github@rainy.me>
2019-10-09 07:50:09 +0900

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Card Stack Demo
+
+Origin: [codesandbox](https://codesandbox.io/embed/j0y0vpz59)
---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import Deck from "./Deck";
+
+export default function App() {
+  return <Deck />;
+}
